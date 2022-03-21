c1aa740643439fc524dbe5f397f5a3db6b27b64e
rishi-dange <78787536+rishi-dange@users.noreply.github.com>
2022-03-21 04:57:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ app = Flask(__name__, template_folder='.')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        sport_c = request.args.get("sport_c")
-        initializer_array = [sport_c, 
+        initializer_array = [request.args.get('sport_c'), 
                             request.args.get('location_c'), 
                             request.args.get('date_c'),
                             request.args.get('start_time_c'),
