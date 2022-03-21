0d4f713b531efff314810ac1cd66b99f326b66b3
Nikhil Ajjarapu <nikhila@princeton.edu>
2022-03-20 21:28:41 -0400

    event.py

--- event.py
+++ event.py
@@ -2,22 +2,38 @@
 # event.py
 # Author: Nikhil Ajjarapu
 #-----------------------------------------------------------------------
-
 class Event:
 
-    def __init__(self, sport, location, datetime):
-        self.sport = sport
-        self.location = location
-        self.datetime = datetime
+    def __init__(self, initializer_array):
+        self._event_id = initializer_array[0]
+        self._sport = initializer_array[1]
+        self._location =  initializer_array[2]
+        self._date = initializer_array[3]
+        self._starttime = initializer_array[4]
+        self._endtime = initializer_array[5]
+        self._visibility = initializer_array[6]
+        self._organizer = initializer_array[7]
+        
+    def get_event_id(self):
+        return self._event_id
 
-    def __str__(self):
-        return self.sport + ', ' + self.location + ', ' + self.datetime
+    def get_sport(self):
+        return self._sport
 
-    def getSport(self):
-        return self.sport
+    def get_location(self):
+        return self._location
 
-    def getLocation(self):
-        return self.location
+    def get_date(self):
+        return self._date
+    
+    def get_starttime(self):
+        return self._starttime
+    
+    def get_endtime(self):
+        return self._endtime
+    
+    def get_visibility(self):
+        return self._visibility
 
-    def getDatetime(self):
-        return self.datetime
+    def get_organizer(self):
+        return self._organizer
