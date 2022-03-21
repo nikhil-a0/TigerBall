22493f80e89bef26826e1fb6ec67021e827c77fe
rishi-dange <78787536+rishi-dange@users.noreply.github.com>
2022-03-21 06:34:27 -0400

    Update database.py

--- database.py
+++ database.py
@@ -61,7 +61,7 @@ def search_event(args_arr):
                 # Create a prepared statement and substitute values.
                 stmt_str = 'SELECT events.event_id, events.sport, events.location, events.event_date, \
                 events.start_time, events.end_time, events.visibility, events.organizer_id \
-                FROM events, eventsparticipants WHERE TRUE'
+                FROM events WHERE TRUE'
                 args_list = []
 
                 # conditional sport
