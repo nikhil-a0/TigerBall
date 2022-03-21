93b3cc3377ed24040cba16e0029a166aacc073a9
rishi-dange <78787536+rishi-dange@users.noreply.github.com>
2022-03-21 00:34:13 -0400

    Create createdb.py

---
+++ createdb.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# createdb.py
+# Author: Bob Dondero, NA/RD/AG
+#-----------------------------------------------------------------------
+
+from sys import argv, stderr, exit
+from psycopg2 import connect
+
+#-----------------------------------------------------------------------
+
+def main():
+
+    if len(argv) != 1:
+        print('Usage: python create.py', file=stderr)
+        exit(1)
+
+    try:
+        with connect(
+            host='localhost', port=5432,
+            database='tigerballdb') as connection:
+
+            with connection.cursor() as cursor:
+
+                #-------------------------------------------------------
+
+                cursor.execute("DROP TABLE IF EXISTS events")
+                cursor.execute("CREATE TABLE events "
+                    + "(event_id SERIAL NOT NULL, sport TEXT, location TEXT,"
+                    + " event_date DATE, start_time TIME, end_time TIME, "
+                    "visibility BOOLEAN, organizer_id TEXT)")
+
+                #-------------------------------------------------------
+
+                cursor.execute("DROP TABLE IF EXISTS eventsparticipants")
+                cursor.execute("CREATE TABLE eventsparticipants "
+                    + "(event_id INTEGER, participant_id TEXT)")
+
+                #-------------------------------------------------------
+
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
