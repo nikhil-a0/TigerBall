01ee3845efb1fb7827e730c37462236cdb96447e
rishi-dange <78787536+rishi-dange@users.noreply.github.com>
2022-03-28 03:01:37 -0400

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,29 @@
+# schema.py
+
+#!/usr/bin/env python
+
+#--------
+# schema.py
+# Author: RD, NA, AG
+#--------
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Events (Base):
+	__tablename__ = 'events'
+	event_id = Column(Integer, primary_key=True, autoincrement=True)
+	sport = Column(String)
+	location = Column(String)
+	event_date = Column(Date)
+	start_time = Column(Time)
+	end_time = Column(Time)
+	visibility = Column(String)
+	organizer = Column(String)
+
+class EventsParticipants (Base):
+	__tablename__ = 'eventsparticipants'
+	event_id = Column(Integer)
+	participant_id = Column(String)
