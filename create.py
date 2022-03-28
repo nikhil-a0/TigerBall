b4aadc25653d5c72b9f7d853c0cdbd632f57817b
rishi-dange <78787536+rishi-dange@users.noreply.github.com>
2022-03-28 03:01:11 -0400

    Create create.py

---
+++ create.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# create.py
+# Author: Bob Dondero, RD/NA/AG
+#-----------------------------------------------------------------------
+
+from sys import argv, stderr, exit
+from sqlite3 import connect as sqlite_connect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from schema import Base, Events, EventsParticipants
+import psycopg2
+from psycopg2 import connect
+
+#-----------------------------------------------------------------------
+
+def main():
+
+    if len(argv) != 1:
+        print('Usage: python create.py', file=stderr)
+        exit(1)
+
+    try:
+        conn = psycopg2.connect(database='postgres',
+            port=5432)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute("DROP DATABASE IF EXISTS tigerballdb")
+        cursor.execute("CREATE DATABASE tigerballdb")
+
+        engine = create_engine('postgresql+psycopg2://@5432/tigerballdb',
+            creator=lambda: psycopg2.connect(database='tigerballdb',
+                port=5432)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+
+        #---------------------------------------------------------------
+
+        event = Events(sport='soccer', location='poe', date='2022-03-21',
+            start_time='14:30:00', end_time='15:30:00', visibility='public',
+            organizer='rdange')
+        session.add(event)
+        session.commit()
+
+        #---------------------------------------------------------------
+
+        session.close()
+        engine.dispose()
+
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
