6687e06b08c4453c059d1730030050d95670bb88
Nikhil Ajjarapu <nikhila@princeton.edu>
2022-04-08 21:00:56 -0400

    postgres heroku test 3

--- db.py
+++ db.py
@@ -18,7 +18,7 @@ from config import ENVIRONMENT_
 if ENVIRONMENT_ == 'dev':
     DATABASE_URL = 'postgresql+psycopg2://@5432/tigerballdb'
 elif ENVIRONMENT_ == 'deploy':
-    DATABASE_URL = 'postgresql+psycopg2://@5432/tigerballdb'
+    DATABASE_URL = 'postgres://fjoacapxjmfqdq:6bc7c2106fefb7d79382461eaa98fe8cab9b686892fd9022c20abcfd88ace07c@ec2-34-207-12-160.compute-1.amazonaws.com:5432/d5olnm6egr5314'
 
 
 #-----------------------------------------------------------------------
