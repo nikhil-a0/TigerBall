df6f4908d01ef8636b92464b20d3b8982d2b28ee
Rishi Dange <rishidange@dynamic-oit-vapornet100-10-9-181-67.princeton.edu>
2022-04-19 02:09:49 -0400

    why

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from keys import APP_SECRET_KEY
 from db import search_event, create_event, get_details, invite_participant,\
     update_event, search_pending_event, update_participant, delete_old_events,\
     get_status_events, create_group, view_groups, get_group_details,\
-    add_to_group, leave_group
+    add_to_group, leave_group, invite_group
 from config import USERNAME_, ENVIRONMENT_, DATABASE_URL
 import os
 from sendgrid import SendGridAPIClient
@@ -308,7 +308,7 @@ def my_events():
         status = 'attending'
 
     events = get_status_events(username, status)
-    for event in events:
+    #for event in events:
         # print(event.get_sport())
         # print(event.get_date())
         # print(event.get_starttime())
