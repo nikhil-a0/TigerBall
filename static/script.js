6dcfbe64ce75a5a15474c2ead6206715f26854ea
agewirtz <sgewirtz@princeton.edu>
2022-05-03 08:38:13 -0400

    mobile compatible

--- static/script.js
+++ static/script.js
@@ -10,3 +10,14 @@
 //     header.classList.remove("sticky");
 //   }
 // }
+
+
+$(window).scroll(function () {
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 63) {
+      $('#myheader').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 64) {
+      $('#myheader').removeClass('navbar-fixed');
+    }
+  });
